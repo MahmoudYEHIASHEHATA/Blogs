ebde3194c1aafab91f975adab8f0b836d7b5594c
Shehatah <mahmoudyehia.shehata@cassbana.com>
2022-11-18 22:42:52 +0200

    small refactor

--- common/src/main/java/com/shahry/common/DateExtension.kt
+++ common/src/main/java/com/shahry/common/DateExtension.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 
 fun Long.getFormatTime(): String? {
-    val currentTime: Date? = Date(this * 1000)
+    val currentTime = Date(this * 1000)
     val timeZoneDate = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
     return timeZoneDate.format(currentTime)
 }
