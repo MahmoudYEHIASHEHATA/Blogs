4f69e5e48ffddbbb0455c49b14ff2175e29a40e5
Shehatah <mahmoudyehia.shehata@cassbana.com>
2022-11-18 22:38:08 +0200

    Add date extention
      +Add :
        + Add extension function to return format date.

---
+++ common/src/main/java/com/shahry/common/DateExtension.kt
@@ -0,0 +1,11 @@
+package com.shahry.common
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun Long.getFormatTime(): String? {
+    val currentTime: Date? = Date(this * 1000)
+    val timeZoneDate = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+    return timeZoneDate.format(currentTime)
+}
