7cdbe6ff2dde2fa49f9be8eb130940b8a2b1b6c0
Shehatah <mahmoudyehia.shehata@cassbana.com>
2022-11-20 09:14:02 +0200

    Add Core Application Class

---
+++ app/src/main/java/com/shahry/blogs/MyApp.kt
@@ -0,0 +1,11 @@
+package com.shahry.blogs
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Core Application Class
+ */
+@HiltAndroidApp
+class MyApp : Application() {
+}
